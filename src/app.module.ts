e076a302a1cc3fbcc04d86fcab5375dc3e4a431b
S4rtox <juandiegourbinamederico@gmail.com>
2025-09-20 23:17:54 -0600

    new Resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProvidersModule } from './providers/providers.module';
+import { ManagersModule } from './managers/managers.module';
+import { LocationsModule } from './locations/locations.module';
+import { RegionsModule } from './regions/regions.module';
 //Base del prouyecto nest, del framework
 @Module({
   imports: [
@@ -25,6 +28,9 @@ import { ProvidersModule } from './providers/providers.module';
     EmployeesModule,
     ProductsModule,
     ProvidersModule,
+    ManagersModule,
+    LocationsModule,
+    RegionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
